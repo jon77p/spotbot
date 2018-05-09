274c679786facc2f21ab70bb5edb9898f0ef7b95
SecLab Spotify Pi <pi@seclab>
2018-05-09 22:15:42 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import spotifyinfo
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    res = spotifyinfo.info()
+    #printed = res[0] + res[1] + res[2]
+    #return printed
+    #return render_template('index.html', res=res)
+    test = render_template('index.html', res=res)
+    return test
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=3000, use_reloader=True)
---
+++ spotifyinfo.py
@@ -0,0 +1,34 @@
+import spotipy
+import sys
+import os
+import pprint
+
+#try:
+#    url = sys.argv[1]
+#except:
+#    print('Error! Expected track URI identifier')
+#    exit()
+
+#url = input().split()[3]
+url = open('/home/pi/raspotify.log', 'r').readlines()[-2].split()[-1][1:-1]
+url = str(url)
+#print(url)
+
+token = os.getenv('SPOTIFY_ACCESS_TOKEN')
+
+spotify = spotipy.Spotify(auth=token)
+
+track = spotify.track(url)
+
+#pprint.pprint(track)
+print 'Track Name:', track["name"]
+print 'Artist:', track["artists"][0]["name"]
+print 'Image:', track["album"]["images"][0]["url"]
+
+def info():
+    url = open('/home/pi/raspotify.log', 'r').readlines()[-2].split()[-1][1:-1]
+    url = str(url)
+    token = os.getenv('SPOTIFY_ACCESS_TOKEN')
+    spotify = spotipy.Spotify(auth=token)
+    track = spotify.track(url)
+    return track["name"], track["artists"][0]["name"], track["album"]["images"][0]["url"]
