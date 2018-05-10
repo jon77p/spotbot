4c95d1efa5a41fe8b46ee73a1ab90b70cc4ebb7c
SecLab Spotify Pi <pi@seclab>
2018-05-10 00:13:15 +0000

    Refactored Spotify Auth workflow and cleaned stuff up

--- spotifyinfo.py
+++ spotifyinfo.py
@@ -2,21 +2,28 @@ import spotipy
 import sys
 import os
 import pprint
+import requests
 
-#try:
-#    url = sys.argv[1]
-#except:
-#    print('Error! Expected track URI identifier')
-#    exit()
-
-#url = input().split()[3]
 url = open('/home/pi/raspotify.log', 'r').readlines()[-1].split()[-1][1:-1]
 url = str(url)
-#print(url)
 
-token = os.getenv('SPOTIFY_ACCESS_TOKEN')
+def tokenreauth():
+    f = open('../secret', 'r').readlines()
+    url = 'https://accounts.spotify.com/api/token'
+    headers = {'Authorization': 'Basic ' + f[0].strip()}
+    data = {'grant_type': 'refresh_token', 'refresh_token': f[1].strip()}
+    req = requests.post(url, headers=headers, data=data)
+    res = req.json()
 
-spotify = spotipy.Spotify(auth=token)
+    return res['access_token']
+
+token = tokenreauth()
+
+try:
+    spotify = spotipy.Spotify(auth=token)
+except:
+    token = tokenreauth()
+    spotify = spotipy.Spotify(auth=token)
 
 track = spotify.track(url)
 
@@ -28,7 +35,11 @@ print 'Image:', track["album"]["images"][0]["url"]
 def info():
     url = open('/home/pi/raspotify.log', 'r').readlines()[-2].split()[-1][1:-1]
     url = str(url)
-    token = os.getenv('SPOTIFY_ACCESS_TOKEN')
-    spotify = spotipy.Spotify(auth=token)
+    try:
+        spotify = spotipy.Spotify(auth=token)
+    except:
+        token = tokenreauth()
+        spotify = spotipy.Spotify(auth=token)
+
     track = spotify.track(url)
     return track["name"], track["artists"][0]["name"], track["album"]["images"][0]["url"]
