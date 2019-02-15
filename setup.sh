cfefe18a4a84b473ddd3cccd91b4fdcca07b857a
Jon Prentice <jon77p@gmail.com>
2019-02-15 11:35:00 -0800

    Added directory finding I think

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo ln -s etc/systemd/system/librespot.service /etc/systemd/system/librespot.service
-sudo ln -s etc/systemd/system/spotbot.service /etc/systemd/system/spotbot.service
-sudo ln -s etc/systemd/system/spotbot_script.service /etc/systemd/system/spotbot_script.service
-sudo ln -s etc/systemd/system/spotbot_script.timer /etc/systemd/system/spotbot_script.timer
+CURRENT="$(pwd)"
+sudo ln -s "$(CURRENT)"/etc/systemd/system/librespot.service /etc/systemd/system/librespot.service
+sudo ln -s "$(CURRENT)"/etc/systemd/system/spotbot.service /etc/systemd/system/spotbot.service
+sudo ln -s "$(CURRENT)"/etc/systemd/system/spotbot_script.service /etc/systemd/system/spotbot_script.service
+sudo ln -s "$(CURRENT)"/etc/systemd/system/spotbot_script.timer /etc/systemd/system/spotbot_script.timer
 sudo systemctl daemon-reload
 sudo systemctl start librespot.service
 sudo systemctl enable librespot.service
@@ -11,6 +12,6 @@ sudo systemctl enable spotbot.service
 sudo systemctl start spotbot_script.service
 sudo systemctl enable spotbot_script.service
 sudo systemctl start spotbot_script.timer
-sudo ln -s etc/nginx/sites-available/spotbot /etc/nginx/sites-available/spotbot
+sudo ln -s "$(CURRENT)"/etc/nginx/sites-available/spotbot /etc/nginx/sites-available/spotbot
 sudo ln -s /etc/nginx/sites-available/spotbot /etc/nginx/sites-enabled/spotbot
 sudo systemctl restart nginx
